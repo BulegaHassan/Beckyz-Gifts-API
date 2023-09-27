5ce9a62b50e98063cfac306dae6c995967afbde3
Hassan Bulega <hassanbulega@gmail.com>
2023-09-27 09:52:40 +0300

    add middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const gifts = require('./routes/gifts')
+// middleware
+ 
+app.use(express.json())
+app.use('/api/v1/gifts', gifts)
 
 const port = 3001;
 app.listen(port, () => {
