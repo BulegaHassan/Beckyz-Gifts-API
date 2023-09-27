1ed12f1c1dc4ab6f627247d267928be6e9bc61e8
Hassan Bulega <hassanbulega@gmail.com>
2023-09-27 12:45:33 +0300

    feat: create and get gifts using the model

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -1,8 +1,20 @@
-const getAllGifts = (req, res) => {
-  res.send("get all gifts");
+const Gift = require('../models/Gift')
+
+const getAllGifts = async (req, res) => {
+  try {
+    const gifts = await Gift.find({})
+    res.status(200).json({gifts, amount:gifts.length})
+  } catch (error) {
+    res.status(500).json({ msg: error });    
+  }
 };
-const CreateGift = (req, res) => {
-  res.send(req.body);
+const CreateGift = async (req, res) => {
+try {
+    const gift = await Gift.create(req.body)
+    res.status(201).json({gift})
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
 };
 const getGift = (req, res) => {
   res.send("get a single gift");
