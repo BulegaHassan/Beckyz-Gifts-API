b7630a3abcba0898c74822f81e630eec79f72d52
Hassan Bulega <hassanbulega@gmail.com>
2023-09-27 14:03:16 +0300

    feat: get a single gift using id

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -1,23 +1,32 @@
-const Gift = require('../models/Gift')
+const Gift = require("../models/Gift");
 
 const getAllGifts = async (req, res) => {
   try {
-    const gifts = await Gift.find({})
-    res.status(200).json({gifts, amount:gifts.length})
+    const gifts = await Gift.find({});
+    res.status(200).json({ gifts, amount: gifts.length });
   } catch (error) {
-    res.status(500).json({ msg: error });    
+    res.status(500).json({ msg: error });
   }
 };
 const CreateGift = async (req, res) => {
-try {
-    const gift = await Gift.create(req.body)
-    res.status(201).json({gift})
+  try {
+    const gift = await Gift.create(req.body);
+    res.status(201).json({ gift });
   } catch (error) {
-    res.status(500).json({msg: error})
+    res.status(500).json({ msg: error });
   }
 };
-const getGift = (req, res) => {
-  res.send("get a single gift");
+const getGift = async (req, res) => {
+  try {
+    const { id: giftID } = req.params;
+    const gift = await Gift.findOne({ _id: giftID });
+    if (!gift) {
+      return res.status(404).json({ msg: `No gift with id ${giftID}` });
+    }
+    res.status(200).json({ gift });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const deleteGift = (req, res) => {
   res.send("delete a gift");
