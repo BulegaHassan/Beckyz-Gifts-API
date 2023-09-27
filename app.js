637d9c5c5575656c4ab1a7287a335d7c141b7918
Hassan Bulega <hassanbulega@gmail.com>
2023-09-27 11:54:52 +0300

    feat: connect to the database connect to mongodb database using mongoose server starts only after DB is connected

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const gifts = require('./routes/gifts')
+const connectDB = require('./db/connect')
 // middleware
  
 app.use(express.json())
 app.use('/api/v1/gifts', gifts)
 
 const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+          console.log(` DB connected and Server is running on port: ${port}`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
