5175461677c9ce4fe8ff794b39b3d3c371b8937a
Hassan Bulega <hassanbulega@gmail.com>
2023-09-30 17:23:41 +0300

    add middleware

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app = express();
 require("dotenv").config();
 const gifts = require('./routes/gifts')
 const connectDB = require('./db/connect')
-// middleware
- 
+require("express-async-errors");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.send('<h1>Gifts API</h1><a href="/api/v1/gifts">gifts route</a>');
+});
 app.use('/api/v1/gifts', gifts)
+
+// middleware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const port = 3001;
 const start = async () => {
