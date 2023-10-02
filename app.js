a81fab8b3d0befc2763e8e93d61471fa0140bcc5
Hassan Bulega <hassanbulega@gmail.com>
2023-10-02 21:47:38 +0300

    feat(middleware)! add authentication check for token in the header

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-require("express-async-errors");
-const app = express();
 require("dotenv").config();
-const connectDB = require('./db/connect')
-
-const giftsRouter = require('./routes/gifts')
-const authRouter = require('./routes/auth')
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const authenticateUser = require('./middleware/authentication')
+const giftsRouter = require("./routes/gifts");
+const authRouter = require("./routes/auth");
 // error handlers
 const notFoundMiddleware = require("./middleware/not-found");
-const errorMiddleware = require("./middleware/error-handler");
-app.use(express.json())
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send('<h1>Gifts API</h1><a href="/api/v1/gifts">gifts route</a>');
 });
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/gifts", giftsRouter);
+app.use("/api/v1/gifts",authenticateUser, giftsRouter);
 
 // middleware
 app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+app.use(errorHandlerMiddleware);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
-          console.log(` DB connected and Server is running on port: ${port}`);
-        });
-    } catch (error) {
-        console.log(error)
-    }
-}
-start()
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(` DB connected and Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
