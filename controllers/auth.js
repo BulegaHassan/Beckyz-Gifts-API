ab62a55258b426dc9b2891fdea51fdbfa6c5d90b
Hassan Bulega <hassanbulega@gmail.com>
2023-10-02 21:54:48 +0300

    feat! add login functionality Authenticate user using their email Check password authenticity and create a token for user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,34 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
-
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user:{name: user.name},token });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
-  // res.status(StatusCodes.CREATED).json(req.body);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
-const deleteUsers = async (req,res) => {
-  const users = await User.deleteMany({})
-  res.status(StatusCodes.OK).json({msg: `Users deleted`})
-}
+const deleteUsers = async (req, res) => {
+  const users = await User.deleteMany({});
+  res.status(StatusCodes.OK).json({ msg: `Users deleted` });
+};
 
-module.exports = { register, login,deleteUsers };
+module.exports = { register, login, deleteUsers };
