96560c4ebb44372aea85b242798da3a7ed29774c
Hassan Bulega <hassanbulega@gmail.com>
2023-10-02 09:31:45 +0300

    add routers

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 require("dotenv").config();
-const gifts = require('./routes/gifts')
 const connectDB = require('./db/connect')
 
+const giftsRouter = require('./routes/gifts')
+const authRouter = require('./routes/auth')
+// error handlers
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 app.use(express.json())
@@ -13,7 +15,9 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send('<h1>Gifts API</h1><a href="/api/v1/gifts">gifts route</a>');
 });
-app.use('/api/v1/gifts', gifts)
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/gifts", giftsRouter);
 
 // middleware
 app.use(notFoundMiddleware);
