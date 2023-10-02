5ba618d5f80b273160df612ea1a05d2ad00105c8
Hassan Bulega <hassanbulega@gmail.com>
2023-10-02 13:55:26 +0300

    refactor: use http-status codes Instead of hard coding status values, we use StatusCodes with http verbs corresponding to values

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -1,28 +1,33 @@
 const Gift = require("../models/Gift");
+const { StatusCodes } = require("http-status-codes");
 
 const getAllGifts = async (req, res) => {
   const gifts = await Gift.find({});
-  res.status(200).json({ gifts, amount: gifts.length });
+  res.status(StatusCodes.OK).json({ gifts, amount: gifts.length });
 };
 const CreateGift = async (req, res) => {
   const gift = await Gift.create(req.body);
-  res.status(201).json({ gift });
+  res.status(StatusCodes.CREATED).json({ gift });
 };
 const getGift = async (req, res) => {
   const { id: giftID } = req.params;
   const gift = await Gift.findOne({ _id: giftID });
   if (!gift) {
-    return res.status(404).json({ msg: `No gift with id ${giftID}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No gift with id ${giftID}` });
   }
-  res.status(200).json({ gift });
+  res.status(StatusCodes.OK).json({ gift });
 };
 const deleteGift = async (req, res) => {
   const { id: giftID } = req.params;
   const gift = await Gift.findOneAndDelete({ _id: giftID });
   if (!gift) {
-    return res.status(404).json({ msg: `No task with id ${giftID}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No task with id ${giftID}` });
   }
-  res.status(200).json({ msg: `Deleted task with id ${giftID}` });
+  res.status(StatusCodes.OK).json({ msg: `Deleted task with id ${giftID}` });
 };
 
 const updateGift = async (req, res) => {
@@ -32,10 +37,12 @@ const updateGift = async (req, res) => {
     runValidators: true,
   });
   if (!gift) {
-    return res.status(404).json({ msg: `No gift with id ${giftID}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No gift with id ${giftID}` });
   }
   res
-    .status(200)
+    .status(StatusCodes.OK)
     .json({ gift, msg: `Gift with id ${giftID} has been updated` });
 };
 
