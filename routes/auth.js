51452161220c0f6063117389bc896ca2c3011f63
Hassan Bulega <hassanbulega@gmail.com>
2023-10-02 14:55:03 +0300

    add router for deleting users

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/auth");
+const { register, login, deleteUsers } = require("../controllers/auth");
 router.post("/register", register);
 router.post("/login", login);
+router.delete('/', deleteUsers)
 
 module.exports = router;
