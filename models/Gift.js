d56db48fbc11290269a5f21bdeb2394f4333d261
Hassan Bulega <hassanbulega@gmail.com>
2023-10-03 09:31:25 +0300

    feat: add timestamps BREAKING CHANGE! automatically provide createdAt and updatedAt fields

--- models/Gift.js
+++ models/Gift.js
@@ -1,58 +1,58 @@
 const mongoose = require("mongoose");
 
-const GiftSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Must provide name"],
-    trim: true,
-    maxLength: [100, "name cannot exceed 100 characters"],
+const GiftSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Must provide name"],
+      trim: true,
+      maxLength: [100, "name cannot exceed 100 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide product price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide product description"],
+      maxlength: [1000, "Description can not be more than 1000 characters"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpg",
+      required: [true, "Please provide image Link"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide gift category"],
+      enum: ["flowers", "ceramics", "jewellery", "others"],
+    },
+    inStock: {
+      type: Boolean,
+      default: false,
+    },
+    colors: {
+      type: [String],
+      default: ["pink"],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
   },
-  price: {
-    type: Number,
-    required: [true, "Please provide product price"],
-    default: 0,
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide product description"],
-    maxlength: [1000, "Description can not be more than 1000 characters"],
-  },
-  image: {
-    type: String,
-    default: "/uploads/example.jpg",
-    required: [true, "Please provide image Link"],
-  },
-  category: {
-    type: String,
-    required: [true, "Please provide gift category"],
-    enum: ["flowers", "ceramics", "jewellery", "others"],
-  },
-  inStock: {
-    type: Boolean,
-    default: false,
-  },
-  colors: {
-    type: [String],
-    default: ["pink"],
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Please provide user"],
-  },
-});
+  { timestamps: true }
+);
 
 GiftSchema.set("toJSON", {
   transform: (document, returnedObject) => {
