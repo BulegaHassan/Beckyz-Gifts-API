dc2b969bf0fe837712e34afb00ba5259ed770c62
Hassan Bulega <hassanbulega@gmail.com>
2023-10-20 08:51:58 +0300

    add getAdmin and deleteUsers routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, deleteUsers } = require("../controllers/auth");
+
+const {
+  register,
+  login,
+  deleteUsers,
+  getAdmins,
+} = require("../controllers/auth");
 router.post("/register", register);
 router.post("/login", login);
+router.get('/',getAdmins)
 router.delete('/', deleteUsers)
 
 module.exports = router;
