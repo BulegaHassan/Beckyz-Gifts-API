0ffb1253ee25419979277ec09c57a673c6d7fe00
Hassan Bulega <hassanbulega@gmail.com>
2023-11-20 23:20:43 +0300

    feat: add pagination

--- controllers/giftsController.js
+++ controllers/giftsController.js
@@ -26,6 +26,8 @@ const getAllGifts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" }; // i is case insestive
   }
   let result = Gift.find(queryObject);
+  const totalDocuments = await Gift.countDocuments(queryObject);
+
   // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
@@ -38,17 +40,33 @@ const getAllGifts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const totalPages = Math.ceil(totalDocuments / limit);
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const gifts = await result;
-  res.status(StatusCodes.OK).json({ gifts, amount: gifts.length });
+  res.status(StatusCodes.OK).json({
+    gifts,
+    meta: {
+      pagination: {
+        page,
+        pageSize: limit,
+        pageCount: totalPages,
+        total: totalDocuments,
+      },
+    },
+  });
 };
 
 // const getAllGiftsStatic = async (req, res) => {
 //   const gifts = await Gift.find({featured: true})
-        // .sort('name') // .sort('-name)  reverse sort
-        // .select('name category featured')
-        // .skip(1)
-        // .limit(11)
+// .sort('name') // .sort('-name)  reverse sort
+// .select('name category featured')
+// .skip(1)
+// .limit(11)
 //   res.status(StatusCodes.OK).json({ gifts, amount: gifts.length });
 // };
 const CreateGift = async (req, res) => {
